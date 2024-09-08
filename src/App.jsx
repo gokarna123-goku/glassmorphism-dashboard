4d200b660c9204cdcc749e50b8bcd3ecf4c223bf
Gokarna Adhikari <agokarna444@gmail.com>
2024-09-08 08:38:42 +0545

    first initialize the dashboard

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+function App() {
+  return (
+    <>
+      <main className="w-full min-h-screen bg-neutral-950 flex flex-col">
+        This is the main section.
+      </main>
+    </>
+  )
+}
+
+export default App
